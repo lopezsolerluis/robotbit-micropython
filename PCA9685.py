b853b208da38774fb05f5fea7a8bf74d135153f4
Luis G. López <lopezsolerluis@gmail.com>
2022-12-06 21:32:03 -0300

    change method names

--- PCA9685.py
+++ PCA9685.py
@@ -20,27 +20,27 @@ _STP_CHD_H = 1023
 class PCA9685:
 
     def __init__(self):
-        self.i2cwrite(_PCA9685_ADDRESS, _MODE1, 0x00)
+        self.write(_PCA9685_ADDRESS, _MODE1, 0x00)
         self.setFreq(50)
         for i in range(0, 16):
             self.setPwm(i, 0, 0)
 
-    def i2cwrite(self, addr, reg, val):
+    def write(self, addr, reg, val):
         i2c.write(addr, bytes([reg, val]))
 
-    def i2cread(self, addr, reg):
+    def read(self, addr, reg):
         i2c.write(addr, bytes([reg]))
         return i2c.read(addr,1)[0]
 
     def setFreq(self, freq):
         prescale = round(25000000.0 / 4096.0 / freq - 1) # since python 3, round returns int.
-        oldmode = self.i2cread(_PCA9685_ADDRESS, _MODE1)
+        oldmode = self.read(_PCA9685_ADDRESS, _MODE1)
         newmode = (oldmode & 0x7F) | 0x10 # sleep
-        self.i2cwrite(_PCA9685_ADDRESS, _MODE1, newmode) # go to sleep
-        self.i2cwrite(_PCA9685_ADDRESS, _PRESCALE, prescale) # set the prescaler
-        self.i2cwrite(_PCA9685_ADDRESS, _MODE1, oldmode)
+        self.write(_PCA9685_ADDRESS, _MODE1, newmode) # go to sleep
+        self.write(_PCA9685_ADDRESS, _PRESCALE, prescale) # set the prescaler
+        self.write(_PCA9685_ADDRESS, _MODE1, oldmode)
         time.sleep_us(5000)
-        self.i2cwrite(_PCA9685_ADDRESS, _MODE1, oldmode | 0xa1)
+        self.write(_PCA9685_ADDRESS, _MODE1, oldmode | 0xa1)
 
     def setPwm(self, channel, on, off):
         i2c.write(_PCA9685_ADDRESS, bytes([_LED0_ON_L + 4 * channel, on & 0xff, (on >> 8) & 0xff, off & 0xff, (off >> 8) & 0xff]))
